cd7526c6291065929c3ed4e04646372c6a433796
Taejeong Kim <taejeongkim98@gmail.com>
2026-04-20 12:25:13 +0900

    MainWindow Added

---
+++ CustomerManagementSystem/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/CMakeFiles/CustomerManagementSystem.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CustomerManagementSystem_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/CustomerManagementSystem_autogen.dir/ParseCache.txt"
+  "CustomerManagementSystem_autogen"
+  "CMakeFiles/CustomerManagementSystem.dir/CustomerManagementSystem_autogen/mocs_compilation.cpp.obj"
+  "CMakeFiles/CustomerManagementSystem.dir/CustomerManagementSystem_autogen/mocs_compilation.cpp.obj.d"
+  "CMakeFiles/CustomerManagementSystem.dir/main.cpp.obj"
+  "CMakeFiles/CustomerManagementSystem.dir/main.cpp.obj.d"
+  "CMakeFiles/CustomerManagementSystem.dir/mainwindow.cpp.obj"
+  "CMakeFiles/CustomerManagementSystem.dir/mainwindow.cpp.obj.d"
+  "CustomerManagementSystem.exe"
+  "CustomerManagementSystem.exe.manifest"
+  "CustomerManagementSystem.pdb"
+  "CustomerManagementSystem_autogen/mocs_compilation.cpp"
+  "CustomerManagementSystem_autogen/timestamp"
+  "libCustomerManagementSystem.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CustomerManagementSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CustomerManagementSystem/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/CMakeFiles/CustomerManagementSystem.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CustomerManagementSystem.
+# This may be replaced when dependencies are built.
