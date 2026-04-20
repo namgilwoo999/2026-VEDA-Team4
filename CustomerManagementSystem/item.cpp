684c1f3e475ad01abd3188eef9f4702a6e7afe04
Taejeong Kim <taejeongkim98@gmail.com>
2026-04-20 16:47:12 +0900

    Item Class Added

--- CustomerManagementSystem/item.cpp
+++ CustomerManagementSystem/item.cpp
@@ -1,3 +1,41 @@
 #include "item.h"
 
-Item::Item() {}
+Item::Item() {
+}
+
+Item::Item(int Id, QString Name, QString Category, int Price) {
+    this->Id = Id;
+    this->Name = Name;
+    this->Category = Category;
+    this->Price = Price;
+}
+
+int Item::GetId() const {
+    return this->Id;
+}
+QString Item::GetName() const {
+    return this->Name;
+};
+QString Item::GetCategory() const {
+    return this->Category;
+}
+int Item::GetPrice() const {
+    return this->Price;
+}
+
+void Item::SetId(int Id) {
+    this->Id = Id;
+}
+
+void Item::SetName(QString Name) {
+    this->Name = Name;
+}
+
+void Item::SetCategory(QString Category) {
+    this->Category = Category;
+}
+
+void Item::SetPrice(int Price) {
+    this->Price = Price;
+}
+
--- CustomerManagementSystem/item.h
+++ CustomerManagementSystem/item.h
@@ -1,10 +1,26 @@
+#include <QString>
 #ifndef ITEM_H
 #define ITEM_H
 
-class Item
-{
+class Item {
+private:
+    int Id;
+    QString Name;
+    QString Category;
+    int Price;
 public:
     Item();
+    Item(int Id, QString Name, QString Category, int Price);
+
+    int GetId() const;
+    QString GetName() const;
+    QString GetCategory() const;
+    int GetPrice() const;
+
+    void SetId(int Id);
+    void SetName(QString Name);
+    void SetCategory(QString Category);
+    void SetPrice(int Price);
 };
 
 #endif // ITEM_H
